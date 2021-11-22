3b82e36a5295452b2e12b70e9e35a0bc972ef0e2
kevinnilsson <kevin7254@gmail.com>
2021-11-22 12:27:24 +0100

    test

--- src/Solver/SudokuApplication.java
+++ src/Solver/SudokuApplication.java
@@ -2,8 +2,5 @@ package Solver;
 
 public class SudokuApplication {
     public static void main(String[] args) {
-        System.out.println("x");
-        String a = "ww";
-        System.out.println(a);
     }
 }
